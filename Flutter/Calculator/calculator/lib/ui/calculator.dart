c7e51d4fc001dd07380deea79265f891407be569
zachary-sears <zears10@gmail.com>
2026-03-13 01:16:07 -0500

    Created a basic display with basic style choices like text size, alignment, and weight. Created a dividing line between display and where button matrix will render.

---
+++ Flutter/Calculator/calculator/lib/ui/calculator.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatelessWidget {
+  const Calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsetsGeometry.fromLTRB(25, 100, 25, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Text',
+                    textAlign: TextAlign.right,
+                    style: TextStyle(fontSize: 75, fontWeight: FontWeight.w300),
+                  ),
+                ],
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+              indent: 25,
+              endIndent: 25,
+              thickness: 2.5,
+              radius: BorderRadius.all(Radius.circular(5)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
