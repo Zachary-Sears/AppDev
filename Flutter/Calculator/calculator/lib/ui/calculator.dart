0cb6060dd8396c097e41377093dcb65d8571249b
zachary-sears <zears10@gmail.com>
2026-03-13 12:18:27 -0500

    Changed calculator file structure, spearated display and button matrix widgets, created all buttons for button matrix

--- Flutter/Calculator/calculator/lib/ui/calculator.dart
+++ Flutter/Calculator/calculator/lib/ui/calculator.dart
@@ -5,36 +5,523 @@ class Calculator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Column(
           children: [
-            Padding(
-              padding: EdgeInsetsGeometry.fromLTRB(25, 100, 25, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    '1+2=3',
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                      fontSize: 75,
-                      fontFamily: 'NanumGothicCoding',
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            Display(),
             Divider(
-              color: Colors.black,
+              color: Colors.deepPurple,
               indent: 15,
               endIndent: 15,
               thickness: 2.5,
               radius: BorderRadius.all(Radius.circular(5)),
             ),
+            ButtonMatrix(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class Display extends StatelessWidget {
+  const Display({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsetsGeometry.fromLTRB(25, 150, 25, 0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            '',
+            textAlign: TextAlign.right,
+            style: TextStyle(fontSize: 75, fontFamily: 'NanumGothicCoding'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonMatrix extends StatelessWidget {
+  const ButtonMatrix({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsetsGeometry.fromLTRB(25, 5, 25, 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  'C',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Icon(Icons.backspace, size: 50),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Icon(Icons.history_rounded, size: 50),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  '/',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsetsGeometry.fromLTRB(25, 5, 25, 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  '1',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  '2',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  '3',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  'X',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsetsGeometry.fromLTRB(25, 5, 25, 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  '4',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  '5',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  '6',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  '+',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsetsGeometry.fromLTRB(25, 5, 25, 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  '7',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  '8',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  '9',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  '-',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsetsGeometry.fromLTRB(25, 5, 25, 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  '+/-',
+                  style: TextStyle(
+                    fontSize: 35,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  '0',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  '.',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>((
+                    Set<WidgetState> states,
+                  ) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.5);
+                    }
+                    return null; // Use the component's default.
+                  }),
+                ),
+                child: Text(
+                  '=',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontFamily: 'NanumGothicCoding',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
